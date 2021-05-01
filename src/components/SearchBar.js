f3a66fc82b4218671f8f6b2aa431c6bb9363e96b
Caroline Visoto Nunes <carolvisoto@gmail.com>
2021-05-01 16:35:52 +1000

    add image search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,18 @@
 import React from 'react';
-class SearchBar extends React.Component{
 
- render(){
-   return(
-    <div>
-        <form>
-           <input />
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input type="text" />
+          </div>
         </form>
-    </div>
-   );
- }
-
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
