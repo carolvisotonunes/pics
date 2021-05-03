ad7f3cbc7bccb6d5581f9b2ba9d4402dab46b916
Caroline Visoto Nunes <carolvisoto@gmail.com>
2021-05-04 00:37:57 +1000

    install axios and view request results

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term){
-    console.log(term);
+  onSearchSubmit(term) {
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:
+          'Client-ID RewOFtjGAbT6XrwlMIZ45tXzMTLzuJxVPvJsXcaVMq0',
+      },
+    });
   }
-  render(){
-      return (
-          <div className="ui container" style={{ marginTop: '10px' }}>
-            <SearchBar onSubmit={this.onSearchSubmit} />
-          </div>
-      );
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
   }
 }
 
